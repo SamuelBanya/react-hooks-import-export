92cd2c5bc1779f6b406d90365be3ea43a8d06ac0
Samuel Banya <sbanya@fastmail.com>
2022-06-23 19:46:49 -0400

    commit tree ea8e5893591db147626babbe3b8d625c378bfd27

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,45 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import HowManyParks from "./parks/HowManyParks";
+import MesaVerde from "./parks/MesaVerde";
+// This is one way to use the idea of 'destructuring' to obtain individual variables from the 'props' object we exported from
+// './parks/RockyMountain'
+// import { trees, wildlife } from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
 
+// We can also rename the variables upon importing them:
+// import {
+  // trees as parkTrees,
+  // wildlife as parkWildLife,
+// } from "./parks/RockyMountain";
+
+// console.log(trees);
+// wildlife();
+
+// We can also use them with their new variable names:
+// console.log(parkTrees);
+// parkWildlife();
+
+console.log(RMFunctions.trees);
+
+RMFunctions.wildlife();
+
+RMFunctions.elevation();
+
+// Same line but written a different way with the 'export default' statement included in the function itself:
+// export default function ColoradoStateParks() {
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  // HowManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  // return <h1>Colorado State Parks!</h1>;
+  return (
+      <div>
+      <MesaVerde />
+      </div>
+  );
 }
+
+export default ColoradoStateParks;
+
+// Alternate method of writing this:
+// export default function ColoradoStateParks() {
+// }
--- src/parks/RockyMountain.js
+++ src/parks/RockyMountain.js
@@ -1,11 +1,22 @@
-const trees = "Aspen and Pine";
+// This is one way to declare 'trees':
+// const trees = "Aspen and Pine";
 
-function wildlife() {
+// This is one way to write the 'wildlife' function and then to export it later:
+// function wildlife() {
+  // console.log("Elk, Bighorn Sheep, Moose");
+// }
+
+
+// We can export the 'props' object via this syntax
+// export { trees, wildlife };
+
+// We can also export the variables on an individual basis:
+export const trees = "Aspen and Pine";
+
+export function wildlife() {
   console.log("Elk, Bighorn Sheep, Moose");
 }
 
-function elevation() {
+export function elevation() {
   console.log("9583 ft");
 }
-
-export { trees, wildlife };
